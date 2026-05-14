f3def699a3b8dbfbc1d9e5f0339b9918827a8a85
Dmitry <146066734+DmitryTamlin7@users.noreply.github.com>
2026-05-14 11:50:04 +0300

    Добавлен бакет минио и проверена жизнеспособность сервисов

---
+++ storing-service/src/main/java/config/RabbitConfig.java
@@ -0,0 +1,35 @@
+package config;
+
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.*;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitConfig {
+
+    @Value("${app.rabbit.queue}")
+    private String queueName;
+
+    @Value("${app.rabbit.exchange}")
+    private String exchangeName;
+
+    @Value("${app.rabbit.routing-key}")
+    private String routingKey;
+
+    @Bean
+    public Queue queue() {
+        return new Queue(queueName, true);
+    }
+
+    @Bean
+    public TopicExchange exchange() {
+        return new TopicExchange(exchangeName);
+    }
+
+    @Bean
+    public Binding binding(Queue queue, TopicExchange exchange) {
+        return BindingBuilder.bind(queue).to(exchange).with(routingKey);
+    }
+}
